8b7decfc9d2aa08caa2b0f564e6954c0e993e0e1
Djihane Ghilani <djihaneghilani@gmail.com>
2026-09-13 18:39:54 +0100

    feat: add convex functions

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,14 +1,16 @@
+import useTheme from "@/hooks/useTheme";
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 const TabsLayout = () => {
+  const { colors } = useTheme();
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "red",
-        tabBarInactiveTintColor: "green",
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.textMuted,
         tabBarStyle: {
-          backgroundColor: "#1e293b",
-          borderTopColor: "yellow",
+          backgroundColor: colors.surface,
+          borderTopColor: colors.border,
           borderTopWidth: 2,
           height: 90,
           paddingBottom: 30,
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import useTheme from "@/hooks/useTheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const { toggleDarkMode } = useTheme();
   return (
     <View style={styles.container}>
       <Text>My App</Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>Toggle the mode</Text>
+      </TouchableOpacity>
     </View>
   );
 }
